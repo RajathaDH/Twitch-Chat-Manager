56e5be8093468fdce795840b28547c8e56a294a0
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-03-13 15:30:14 +0530

    Added messages to DOM

--- client/main.js
+++ client/main.js
@@ -1,10 +1,32 @@
+const messagesDiv = document.querySelector('#messages');
+
+const messages = [];
+
 const client = new tmi.Client({
     connection: { reconnect: true },
-    channels: ['polen']
+    channels: ['']
 });
 
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
     console.log(`${tags['display-name']}: ${message}`);
+
+    createMessage(tags['display-name'], message);
 });
+
+function createMessage(username, message) {
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('message');
+
+    const usernamePara = document.createElement('p');
+    usernamePara.textContent = username;
+
+    const messagePara = document.createElement('p');
+    messagePara.textContent = message;
+
+    messageDiv.appendChild(usernamePara);
+    messageDiv.appendChild(messagePara);
+
+    messagesDiv.appendChild(messageDiv);
+}
